4fb6d961d40ad1f3faeb1485466df03c400db498
alexanderkaptsov <kaptsovaa@gmail.com>
2022-06-29 21:40:50 +0400

    Integrate livereload.

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-import os
+import json
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
+
+
+def rebuild():
+
+    with open("library/library.json", "r") as my_file:
+        library_json = my_file.read()
+
+    cards = json.loads(library_json)
+
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+
+    rendered_page = template.render(cards=cards)
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+    print("Site rebuilt")
 
 
 def main():
-    pass
+
+    rebuild()
+    server = Server()
+    server.watch('index.html', rebuild)
+    server.serve(root='.')
 
 
 if __name__ == '__main__':
     main()
-
-
